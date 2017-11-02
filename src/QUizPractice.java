b86252e7643e9ada042ca40fe4e00020929ea6d4
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-11-02 14:09:56 -0400

    First Commit

---
+++ src/QUizPractice.java
@@ -0,0 +1,65 @@
+
+public class QUizPractice
+{
+
+	public static void main(String[] args) 
+	{
+		/*int x = 0;
+		if (x < 4) {
+		  x = x + 1;
+		}
+		System.out.println("x is " + x);
+		boolean even = false;
+		if (even) {
+		  System.out.println("It is even!");
+		}
+		else
+			 System.out.println("It is false!");
+		 int number = 6;
+		    while (number > 0) {
+		      number -= 3;
+		      System.out.print(number + " ");
+		    }
+		    */
+		/*int max = 0;
+		  if (max != 0)
+		    System.out.println(max);
+		  else
+		    return;
+		    */
+		/*int number = 45;
+		boolean even;
+
+		if (number % 2 == 0) 
+		  even = true;
+		else 
+		  even = false;
+		*/
+		int x = 3; int y = 4;
+		switch (x + 3) {
+		  case 6: y = 0;
+		  
+		  case 7: y = 1;
+		  break;
+		  default: y += 1;
+		}
+		System.out.print(y);
+		 char ch = 'b';
+		    
+		    switch (ch) {
+		      case 'a':
+		        System.out.print(ch);
+		      case 'c':
+		        System.out.print(ch);
+		      case 'b':
+		        System.out.print(ch);
+		      case 'd':
+		        System.out.print(ch);
+		    }
+		    boolean even = false;
+		    System.out.println((even ? 5 : 6));
+		    System.out.printf("%3.1e", 1234.56);
+		 
+	}
+
+}
